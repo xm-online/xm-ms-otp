8ce9840a02c7752a7261920dd58a746d24aba96e
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-11-23 13:17:03 +0200

    refactoring

--- src/main/java/com/icthh/xm/ms/otp/domain/enumeration/ReceiverTypeKey.java
+++ src/main/java/com/icthh/xm/ms/otp/domain/enumeration/ReceiverTypeKey.java
@@ -1,8 +1,26 @@
 package com.icthh.xm.ms.otp.domain.enumeration;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * The ReceiverTypeKey enumeration.
  */
 public enum ReceiverTypeKey {
-    USER_ID, EMAIL, IP, NAME, PHONE_NUMBER
+
+    USER_ID("USER-ID"),
+    EMAIL("EMAIL"),
+    IP("IP"),
+    NAME("NAME"),
+    PHONE_NUMBER("PHONE-NUMBER");
+
+    private final String value;
+
+    ReceiverTypeKey(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
