3979d09c2466c3966eed4b524e56fc6606613afa
Andrew Koidan <deathangel908@gmail.com>
2018-11-23 10:59:38 +0200

    Added lep and permissions

---
+++ src/main/java/com/icthh/xm/ms/otp/config/AppResourceFactory.java
@@ -0,0 +1,14 @@
+package com.icthh.xm.ms.otp.config;
+
+
+import com.icthh.xm.commons.permission.access.ResourceFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppResourceFactory implements ResourceFactory {
+
+    @Override
+    public Object getResource(Object resourceId, String objectType) {
+        return null;
+    }
+}
--- src/main/java/com/icthh/xm/ms/otp/config/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/ms/otp/config/ApplicationProperties.java
@@ -1,6 +1,12 @@
 package com.icthh.xm.ms.otp.config;
 
+import com.icthh.xm.commons.lep.TenantScriptStorage;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Properties specific to Otp.
@@ -8,7 +14,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Properties are configured in the application.yml file.
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String kafkaSystemTopic;
+    private String kafkaSystemQueue;
 
+
+    private final Lep lep = new Lep();
+
+    private List<String> tenantIgnoredPathList = Collections.emptyList();
+
+    @Getter
+    @Setter
+    public static class Lep {
+        private TenantScriptStorage tenantScriptStorage;
+        private String lepResourcePathPattern;
+    }
 }
