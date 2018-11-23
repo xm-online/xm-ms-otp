7c6ef1c5898e9618c0531b1573472b7e78af4e00
Andrew Koidan <deathangel908@gmail.com>
2018-11-23 15:22:56 +0200

    Fixed test so it works

--- src/test/java/com/icthh/xm/ms/otp/web/rest/OneTimePasswordResourceIntTest.java
+++ src/test/java/com/icthh/xm/ms/otp/web/rest/OneTimePasswordResourceIntTest.java
@@ -1,8 +1,14 @@
 package com.icthh.xm.ms.otp.web.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.icthh.xm.ms.otp.OtpApp;
 import com.icthh.xm.ms.otp.config.SecurityBeanOverrideConfiguration;
 import com.icthh.xm.ms.otp.config.tenant.WebappTenantOverrideConfiguration;
+import com.icthh.xm.ms.otp.domain.enumeration.ReceiverTypeKey;
+import com.icthh.xm.ms.otp.service.dto.OneTimePasswordDTO;
 import com.icthh.xm.ms.otp.service.impl.OneTimePasswordServiceImpl;
 import com.icthh.xm.ms.otp.web.rest.errors.ExceptionTranslator;
 import lombok.SneakyThrows;
@@ -20,12 +26,14 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.Validator;
 
 import java.nio.charset.Charset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,6 +52,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     WebappTenantOverrideConfiguration.class
 })
 public class OneTimePasswordResourceIntTest {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+        MediaType.APPLICATION_JSON.getType(),
+        MediaType.APPLICATION_JSON.getSubtype(),
+        Charset.forName("utf8")
+    );
 
     private MockMvc restMockMvc;
 
@@ -64,9 +78,8 @@ public class OneTimePasswordResourceIntTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-
-        OneTimePasswordResource otp = new OneTimePasswordResource(new OneTimePasswordServiceImpl(null, null));
-
+        OneTimePasswordServiceImpl oneTimePswd = new OneTimePasswordServiceImpl(null, null);
+        OneTimePasswordResource otp = new OneTimePasswordResource(oneTimePswd);
         this.restMockMvc = MockMvcBuilders.standaloneSetup(otp)
             .setCustomArgumentResolvers(pageableArgumentResolver)
             .setControllerAdvice(exceptionTranslator)
@@ -76,13 +89,31 @@ public class OneTimePasswordResourceIntTest {
 
     @Test
     public void getUserSubscriptionInformation() throws Exception {
+
+        OneTimePasswordDTO tdo = new OneTimePasswordDTO();
+        tdo.setId(23L);
+        tdo.setReceiver("receiver");
+        tdo.setReceiverTypeKey(ReceiverTypeKey.USER_ID);
+        tdo.setTypeKey("typeKey");
+        String requestJson = toJson(tdo);
+
+        MockHttpServletRequestBuilder postContent = post("/api/one-time-password")
+            .contentType(APPLICATION_JSON_UTF8)
+            .content(requestJson);
         MvcResult result = restMockMvc
-            .perform(post("/api/one-time-password"))
+            .perform(postContent)
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
             .andReturn();
 
         log.info(result.getResponse().getContentAsString());
+    }
+
+    private String toJson(OneTimePasswordDTO tdo) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(tdo );
     }
 
 }
