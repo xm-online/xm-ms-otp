5815780e0bd1083b38bb95ffd078090b2cbdc9ed
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-11-28 12:51:02 +0200

    refactoring

--- src/main/java/com/icthh/xm/ms/otp/domain/enumeration/StateKey.java
+++ src/main/java/com/icthh/xm/ms/otp/domain/enumeration/StateKey.java
@@ -1,23 +1,8 @@
 package com.icthh.xm.ms.otp.domain.enumeration;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum StateKey {
-
-    ACTIVE("ACTIVE"),
-    VERIFIED("VERIFIED"),
-    NOT_VALID("NOT-VALID"),
-    EXPIRED("EXPIRED");
-
-    private final String value;
-
-    StateKey(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
-
+    ACTIVE,
+    VERIFIED,
+    NOT_VALID,
+    EXPIRED;
 }
