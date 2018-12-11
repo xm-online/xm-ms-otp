f2789c1f50a6ab7abc5f09acbe047f486ec4e88e
Andrew Koidan <deathangel908@gmail.com>
2018-12-11 12:06:22 +0200

    Changed password authorization to client credentials

--- src/main/java/com/icthh/xm/ms/otp/domain/UaaConfig.java
+++ src/main/java/com/icthh/xm/ms/otp/domain/UaaConfig.java
@@ -1,10 +1,8 @@
 package com.icthh.xm.ms.otp.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UaaConfig {
     private String systemAuthUrl;
     private String systemUsername;
