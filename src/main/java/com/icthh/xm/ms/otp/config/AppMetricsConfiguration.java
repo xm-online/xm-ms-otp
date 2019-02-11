760ab4fad601ec0c35ba95908b283aa5df1d62cd
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2019-02-11 16:52:29 +0200

    oracle db support

---
+++ src/main/java/com/icthh/xm/ms/otp/config/AppMetricsConfiguration.java
@@ -0,0 +1,43 @@
+package com.icthh.xm.ms.otp.config;
+
+import com.codahale.metrics.MetricRegistry;
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMetrics(proxyTargetClass = true)
+public class AppMetricsConfiguration extends MetricsConfigurerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(AppMetricsConfiguration.class);
+
+    private final MetricRegistry metricRegistry;
+
+    private HikariDataSource hikariDataSource;
+
+    public AppMetricsConfiguration(MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
+    }
+
+    @Autowired(required = false)
+    public void setHikariDataSource(HikariDataSource hikariDataSource) {
+        this.hikariDataSource = hikariDataSource;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (hikariDataSource != null) {
+            log.debug("Monitoring the datasource");
+            // remove the factory created by HikariDataSourceMetricsPostProcessor until JHipster migrate to Micrometer
+            hikariDataSource.setMetricsTrackerFactory(null);
+            hikariDataSource.setMetricRegistry(metricRegistry);
+        }
+    }
+}
