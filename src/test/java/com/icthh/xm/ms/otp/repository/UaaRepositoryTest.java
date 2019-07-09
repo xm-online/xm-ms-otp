f8862cd29449f12ceb1a334261393058c9e454a8
Igor Bilous <igor.bilous@metricly.com>
2019-07-09 18:40:02 +0300

    add MockRestServiceServer for test with rest logic

--- src/test/java/com/icthh/xm/ms/otp/repository/UaaRepositoryTest.java
+++ src/test/java/com/icthh/xm/ms/otp/repository/UaaRepositoryTest.java
@@ -6,37 +6,40 @@ import com.icthh.xm.ms.otp.OtpApp;
 import com.icthh.xm.ms.otp.config.Constants;
 import com.icthh.xm.ms.otp.config.SecurityBeanOverrideConfiguration;
 import com.icthh.xm.ms.otp.config.tenant.WebappTenantOverrideConfiguration;
-import io.github.jhipster.config.JHipsterProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
-import org.thymeleaf.util.StringUtils;
+import org.springframework.web.client.support.RestGatewaySupport;
 
 import java.util.Map;
 import java.util.UUID;
 
 import static com.icthh.xm.ms.otp.config.Constants.ACCESS_TOKEN;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.queryParam;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -51,43 +54,50 @@ public class UaaRepositoryTest {
     @Autowired
     UaaRepository uaaRepository;
 
+    @Autowired
     @Qualifier("internalRestTemplate")
-    @MockBean
     private RestTemplate restTemplate;
 
+    MockRestServiceServer mockServer;
+
+    @Before
+    public void beforeClass(){
+        RestGatewaySupport gateway = new RestGatewaySupport();
+        gateway.setRestTemplate(restTemplate);
+        mockServer = MockRestServiceServer.createServer(gateway);
+    }
 
     @Test
     public void oauthTokenShouldBeRetrievedFromUaaTest() {
 
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), any(ParameterizedTypeReference.class)))
-            .thenReturn(new ResponseEntity(Maps.newHashMap(), HttpStatus.OK));
+        mockServer.expect(method(HttpMethod.POST))
+            .andExpect(queryParam("key1", "value1"))
+            .andExpect(queryParam("key2", "value2"))
+            .andExpect(header(Constants.X_TENANT, "xm"))
+            .andExpect(content().string(containsString("grant_type")))
+            .andExpect(content().string(containsString("username")))
+            .andExpect(content().string(containsString("password")))
+        .andRespond(withSuccess("{\"access_token\" : \"123\"}".getBytes(), MediaType.APPLICATION_JSON));
 
         Map<String, Object> params = ImmutableMap.of("key1", "value1", "key2", "value2");
-        uaaRepository.getOAuth2Token(params);
-
-        ArgumentCaptor<String> urlArgument = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<HttpEntity> entityArgument = ArgumentCaptor.forClass(HttpEntity.class);
-        verify(restTemplate).exchange(urlArgument.capture(), eq(HttpMethod.POST), entityArgument.capture(), any(ParameterizedTypeReference.class));
-        String url = urlArgument.getValue();
-        HttpEntity entity = entityArgument.getValue();
-        assertTrue(StringUtils.endsWith(url, "?key1=value1&key2=value2"));
-        assertNotNull(entity.getHeaders().get(Constants.X_TENANT));
-        Map<String, Object> body = (Map<String, Object>) entity.getBody();
-        assertNotNull(body.get("grant_type"));
-        assertNotNull(body.get("username"));
-        assertNotNull(body.get("password"));
+        String oAuth2Token = uaaRepository.getOAuth2Token(params);
+        assertEquals(oAuth2Token, "123");
+        mockServer.reset();
     }
 
 
     @Test
     public void requestShouldBeSendWithEmptyDetailsMap() {
 
-        String token = UUID.randomUUID().toString();
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), any(ParameterizedTypeReference.class)))
-            .thenReturn(new ResponseEntity(ImmutableMap.of(ACCESS_TOKEN, token), HttpStatus.OK));
+        mockServer.expect(method(HttpMethod.POST))
+            .andExpect(header(Constants.X_TENANT, "xm"))
+            .andExpect(content().string(containsString("grant_type")))
+            .andExpect(content().string(containsString("username")))
+            .andExpect(content().string(containsString("password")))
+            .andRespond(withSuccess("{\"access_token\" : \"123\"}".getBytes(), MediaType.APPLICATION_JSON));
 
         String accessToken = uaaRepository.getOAuth2Token(Maps.newHashMap());
-        assertEquals(accessToken, token);
+        assertEquals(accessToken, "123");
 
     }
 
